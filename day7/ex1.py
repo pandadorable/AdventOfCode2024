d3f56fb225aa72d489c3cefbae8ecadb140d17be
Pandadorable <geffroy250@gmail.com>
2024-12-08 12:16:29 +0100

    day7 and day 8

---
+++ day7/ex1.py
@@ -0,0 +1,15 @@
+input = open("./day7/input.txt")
+sum = 0
+for l in input.readlines():
+    result,values = l.split(':')
+    result = int(result)
+    values = list(map(int,values.split()))
+    #print(result,values)
+    combination = [values[0]]
+    for x in range(1,len(values)):
+        nextList = [values[x]+v for v in combination]+[values[x]*v for v in combination]
+        combination = nextList
+        #print(combination)
+    if result in combination:
+        sum += result
+print(sum)
---
+++ day8/ex2.py
@@ -0,0 +1,28 @@
+input = open("./day8/input.txt")
+ANTENNA = {}
+GRID = [list(l.strip('\n')) for l in input.readlines()]
+for i in range(len(GRID)):
+    for j in range(len(GRID[i])):
+        if GRID[i][j] != '.':
+            if GRID[i][j] not in ANTENNA.keys():
+                ANTENNA[GRID[i][j]] = []
+            ANTENNA[GRID[i][j]].append([i,j])
+
+for k,v in ANTENNA.items():
+    print(k)
+    for a in v:
+        for b in v:
+            if a != b :
+                dir = [-(b[0]-a[0]),-(b[1]-a[1])]
+                step = 0
+                pos = [a[0]+step*dir[0],a[1]+step*dir[1]]
+                while(pos[0] >= 0 and pos[0] < len(GRID) and pos[1] >=0 and pos[1] < len(GRID[0])):
+                    GRID[pos[0]][pos[1]] = '#'
+                    step += 1
+                    pos = [a[0]+step*dir[0],a[1]+step*dir[1]]
+                    
+sum = 0
+for i in GRID:
+    print(''.join(i))
+    sum += i.count('#')
+print(sum)
